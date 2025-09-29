848f924ff987c768807c640bd9b837b2b8f140d5
Maxim Gubanov <m.gubanov@tehnomer.ru>
2025-09-29 16:52:30 +0300

    Исправлена ошибка с открытием файла

--- checking_file.go
+++ checking_file.go
@@ -30,7 +30,7 @@ func CheckingFileExistence(logPath, filename string) (*os.File, error) {
 		file, err = os.OpenFile(logPath+logFileName, os.O_WRONLY|os.O_APPEND, 0666)
 		if err != nil {
 			if os.IsNotExist(err) {
-				file, err = os.OpenFile(logPath+logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+				file, err = os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 				if err == nil {
 					writeLogHeader(file, filename)
 				}
